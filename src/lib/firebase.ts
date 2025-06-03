50bab06d0c21a8eaa62e398f739e9ae60c2e5b99
Gurdev <gur13dev@gmail.com>
2025-06-03 13:00:08 +0530

    added firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// src/lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+
+// Auto sign-in anonymously
+signInAnonymously(auth).catch(console.error);
